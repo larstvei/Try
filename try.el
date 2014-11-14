d0e1e8d3660dbce24f14fd42e9034ff7d1acaca9
larstvei <larstvei@ifi.uio.no>
2014-11-14 23:36:02 +0100

    Added README to commentary.

--- try.el
+++ try.el
@@ -3,8 +3,10 @@
 ;; Copyright (C) 2014 Lars Tveito.
 
 ;; Author: Lars Tveito <larstvei@ifi.uio.no>
-;; Created: 13th November 2014
 ;; URL: http://github.com/larstvei/try
+;; Created: 13th November 2014
+;; Keywords: packages
+;; Version: 0.0.1
 
 ;; Contains code from GNU Emacs <https://www.gnu.org/software/emacs/>,
 ;; released under the GNU General Public License version 3 or later.
@@ -20,6 +22,47 @@
 ;;
 ;; You should have received a copy of the GNU General Public License along
 ;; with Try. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; # Try
+
+;; Try is a packages that allow you to try out Emacs packages without
+;; installing them. If you pass a URL to a plain-text `.el`-file it evaluates
+;; the content, without storing the file.
+
+;; Packages from ELPA will temporarily be stored in `/tmp/` by default.
+
+;; ## Usage
+
+;; Remember to make packages from your `package-archives` available for
+;; download with
+
+;; ```
+;; M-x package-refresh-contents
+;; ```
+
+;; ### Example
+
+;; If you for instance have [melpa](http://melpa.org/) in your
+;; `package-archives` you can try
+;; [multiple cursors](https://github.com/magnars/multiple-cursors.el) by
+;; running:
+
+;; ```
+;; M-x try RET multiple-cursors RET
+;; ```
+
+;; If you on the other hand want to test out a single `.el`-file from somewhere
+;; you can simply insert an URL. Trying out
+;; [Leuven-theme](https://github.com/fniessen/emacs-leuven-theme) can be done
+;; by running:
+
+;; ```
+;; M-x try RET https://raw.githubusercontent.com/fniessen/emacs-leuven-theme/master/leuven-theme.el RET
+;; ```
+
+;; Unfortunately, you won't be able to try Try with `M-x try RET try`.
 
 ;;; Code:
 (require 'package)
