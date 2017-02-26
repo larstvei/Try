larstvei/Try

commit 2046f582ce233b1f7dd5159c3835fa050447addf

Inhibit storing of packages in `package-selected-packages'
Fix #3.

try.el
@@ -87,7 +87,8 @@ to a raw .el file. Packages are stored in `try-tmp-dir' and raw
          (package-symbol (intern url-or-package)))
     (cond ((try-raw-link-p url-or-package) (try-raw-link url-or-package))
           ((try-package-exists-p package-symbol)
-           (let ((package-user-dir try-tmp-dir)
+           (let ((package-selected-packages nil) ; Don't store packages
+                 (package-user-dir try-tmp-dir)
                  (package-alist nil))
              (package-install package-symbol)
              (message "Trying %s!" url-or-package)))

commit 271b0a362cadf44d0694628b9e213f54516ef913

Fix bug introduced by 2046f58
This should provide a fix for #3, and not effect the init.el file.

try.el
@@ -87,10 +87,11 @@ to a raw .el file. Packages are stored in `try-tmp-dir' and raw
          (package-symbol (intern url-or-package)))
     (cond ((try-raw-link-p url-or-package) (try-raw-link url-or-package))
           ((try-package-exists-p package-symbol)
-           (let ((package-selected-packages nil) ; Don't store packages
-                 (package-user-dir try-tmp-dir)
+           (let ((package-user-dir try-tmp-dir)
                  (package-alist nil))
-             (package-install package-symbol)
+             (if (version< emacs-version "25.1")
+                 (package-install package-symbol)
+                 (package-install package-symbol 'dont-select))
              (message "Trying %s!" url-or-package)))
           (t (message (concat "Couldn't find a sensible way to try this. "
                               "Try running `package-refresh-contents'!"))))))